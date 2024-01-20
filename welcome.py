90ef6a0343cfac11d5830d25f9ad569b672f1cf1
Joan <joancayala@gmail.com>
2024-01-20 09:06:40 -0500

    Add new feature: age calc

--- welcome.py
+++ welcome.py
@@ -1,6 +1,9 @@
 name = "Joan"
 country = "Colombia"
+birthday = 1989
+now = 2024
 
 print("You're", name, "and you're from", country)
 print("You're", name)
 print("You're from", country)
+print("Your age is", now-birthday)
