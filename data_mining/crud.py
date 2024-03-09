0aa23094ebf5a40143a109691eaece43669eae2b
Joan <joancayala@gmail.com>
2024-03-09 11:25:08 -0500

    Update crud file

--- data_mining/crud.py
+++ data_mining/crud.py
@@ -10,55 +10,57 @@ import os
 import bcrypt
 
 status_menu = True
-global status_opt
+global status_op
 
 def hash_password(passwd):
     return bcrypt.hashpw(passwd.encode(), bcrypt.gensalt())
 
-def crud(op):
-    if op == '1':
-        #Create a new user
-        os.system('clear')
+def create_user(op):
+    #Create a new user
+    os.system('clear')
 
-        print("::: Signup form :::")
-        fname = input("Your firstname: ")
-        lname = input("Your lastname: ")
-        ident = input("Your ident number: ")
-        email = input("Your email: ")
-        passwd = input("Your password: ")
-        passwd_hashed = hash_password(passwd)
+    print("::: Signup form :::")
+    fname = input("Your firstname: ")
+    lname = input("Your lastname: ")
+    ident = input("Your ident number: ")
+    email = input("Your email: ")
+    passwd = input("Your password: ")
+    passwd_hashed = hash_password(passwd)
 
-        new_user = f'''
-            INSERT INTO 
-                users (firstname, lastname, ident_number, email, password) 
-                VALUES('{fname}', '{lname}', '{ident}', '{email}', "{passwd_hashed}")
-        '''
-        con.execute(new_user)
-        con.commit()
+    new_user = f'''
+        INSERT INTO 
+            users (firstname, lastname, ident_number, email, password) 
+            VALUES('{fname}', '{lname}', '{ident}', '{email}', "{passwd_hashed}")
+    '''
+    con.execute(new_user)
+    con.commit()
 
-        print("::: New user has been created sucessfully :::")
-        os.system('pause')
-        menu()
-    elif op == '2':
-        #Read all the data from table users
-        os.system('clear')
-        cur.execute("SELECT * FROM users")
-        print(cur.fetchall())
+    print("::: New user has been created sucessfully :::")
+    os.system('pause')
+    menu()
 
-        os.system('pause')
-        menu()
-    elif op == '3':
-        os.system('clear')
-        print("::: SEARCH USER BY IDENTIFICATION NUMBER :::")
-        ident_num = input("Identification number: ")
+def list_users(op):
+    #Read all the data from table users
+    os.system('clear')
+    cur.execute("SELECT * FROM users")
+    print(cur.fetchall())
 
-        cur.execute(f"SELECT email FROM users WHERE ident_number = '{ident_num}' ")
-        print(cur.fetchall())
+    os.system('pause')
+    menu()
 
-        os.system('pause')
-        menu()
+def search_user(op):
+    os.system('clear')
+    print("::: SEARCH USER BY IDENTIFICATION NUMBER :::")
+    ident_num = input("Identification number: ")
+
+    cur.execute(f"SELECT email FROM users WHERE ident_number = '{ident_num}' ")
+    print(cur.fetchall())
+
+    os.system('pause')
+    menu()
 
 def menu():
+    global opt
     status_opt = True
     while status_menu: 
         os.system('clear')
@@ -79,12 +81,16 @@ def menu():
             else :
                 status_opt = False
 
-        if opt == '6':
-            print("::: See 'u soon")
-            break
-        else:
-            crud(opt)
-
+        if opt == '1':
+            create_user(opt)
+        elif opt == '2':
+            list_users(opt)
+        elif opt == '3':
+            search_user(opt)
+        elif opt == '6':
+            print("::: See 'u soon :::")
+            exit()
+        
 menu()
 
 #Close connection
